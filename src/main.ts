0fe708330927ac1ff3bb442a7f1992b7d8dd6f36
vitaliiurban <vitalii.urban.ukr@gmail.com>
2024-01-11 21:59:47 +0100

    build project

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+// @ts-ignore
 import VueAwesomePaginate from 'vue-awesome-paginate'
 const app = createApp(App)
 
